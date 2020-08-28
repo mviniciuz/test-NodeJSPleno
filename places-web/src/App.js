7f2c5cd165589908bc60444e2fcc82d8600efa33
mviniciuz <m.viniciuz@hotmail.com>
2020-08-27 22:15:21 -0300

    maps

---
+++ places-web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/App.css';
+
+import Maps from './pages/maps';
+
+function App() {
+  return (
+    <>
+      <Maps />
+    </>
+  );
+}
+
+export default App;
---
+++ places-web/src/pages/maps/index.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react'
+import { GoogleMap, LoadScript, MarkerClusterer, Marker } from '@react-google-maps/api';
+
+import data from '../../data.js';
+
+const containerStyle = {
+  width: '100%',
+  height: '100vh'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function Maps() {
+
+  const locations = data;
+
+  function createKey(location) {
+    return location.lat + location.lng
+  }
+
+  const options = {
+    imagePath:
+      'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m', 
+  }
+
+  return (
+    <LoadScript
+      googleMapsApiKey="AIzaSyB5x50duuKk7RJ-eiUq05-BT9xuzUM_PAs"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={8}
+      >
+
+        <MarkerClusterer options={options}>
+          {(clusterer) =>
+            locations.map((location) => (
+              <Marker key={createKey(location)} position={location} clusterer={clusterer} />
+            ))
+          }
+        </MarkerClusterer>
+
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
+}
+
+export default Maps;
